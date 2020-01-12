047512cda67bb9aa0a75ea008befcf39f951f3d4
kotwys <52920928+kotwys@users.noreply.github.com>
2020-01-12 15:14:12 +0400

    fix: Interpret image size properly

--- src/bytes.cpp
+++ src/bytes.cpp
@@ -1,9 +1,11 @@
 #include <cstdint>
 
 uint8_t read_l8(const char *ptr) {
+  auto *uptr = reinterpret_cast<const uint8_t*>(ptr);
   return *ptr;
 }
 
 uint16_t read_l16(const char *ptr) {
-  return *ptr | *(ptr + 1) << 8;
+  auto *uptr = reinterpret_cast<const uint8_t*>(ptr);
+  return *uptr | *(uptr + 1) << 8;
 }
--- src/headers/bytes.h
+++ src/headers/bytes.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <cstdint>
 
-inline uint8_t read_l8(const char *ptr);
-inline uint16_t read_l16(const char *ptr);
+uint8_t read_l8(const char *ptr);
+uint16_t read_l16(const char *ptr);
